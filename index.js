d9a5f66c2789781d7ee94ea5841aab3e31b8b10a
Bello Osagie <osagienoah04@gmail.com>
2019-06-29 14:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (name === "Susan"){
 }else{
   console.log("Expected the name to be Susan");
 }
-//don't forget to learn.co
+//don't forget to learn submit
 
 // Don't worry about this
 module.exports = { name, height
