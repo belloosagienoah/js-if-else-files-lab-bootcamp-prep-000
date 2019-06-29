ccf290213f7f410dd7e68f931908df61c2728804
Bello Osagie <osagienoah04@gmail.com>
2019-06-29 14:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (name === "Susan"){
 }else{
   console.log("Expected the name to be Susan");
 }
-//use node index.js or node index to run 
+//use node index.js or node index to run your code
 
 // Don't worry about this
 module.exports = { name, height
