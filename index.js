5c914dfa67dd1271d216b2c06b87e35f39676c44
Bello Osagie <osagienoah04@gmail.com>
2019-06-29 14:52:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 if (name === "Susan"){
   console.log("The name is correct");
 }else{
-  console.log("Expected the name ")
+  console.log("Expected the name to be Sus")
 }
 
 // Don't worry about this
